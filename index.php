65bb33a4e082e2635358d2144016d8290c398420
crash1604 <39547126+crash1604@users.noreply.github.com>
2019-06-13 16:15:40 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,113 @@
+<html>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+<style>
+h1{	font-family:verdana;
+	color: #003366;
+	overflow:auto;
+	}
+	
+body {margin:0;}
+#im{float:right;}
+.fa {
+  padding: 10px;
+  font-size: 30px;
+  width: 30px;
+  text-align: center;
+  text-decoration: none;
+  margin: 5px 2px;
+  border-radius: 50%;
+}
+
+.fa:hover {
+    opacity: 0.7;
+}
+
+.fa-facebook {
+  background: #3B5998;
+  color: white;
+}
+
+.fa-twitter {
+  background: #55ACEE;
+  color: white;
+}
+
+.fa-google {
+  background: #dd4b39;
+  color: white;
+}
+
+.fa-linkedin {
+  background: #007bb5;
+  color: white;
+}
+.icon-bar {
+  width: 100%;
+  background-color: #003366;
+  overflow: auto;
+}
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+}
+
+.icon-bar a {
+  float: left;
+  width: 20%;
+  text-align: center;
+  padding: 12px 0;
+  transition: all 0.3s ease;
+  color: white;
+  font-size: 36px;
+}
+
+li {
+  float: left;
+}
+
+
+.icon-bar a:hover {
+  background-color: #003366;
+}
+
+.active {
+  background-color: #003366;
+}
+</style>
+<body>
+<div>
+<ul>
+<li><h1>MyPage</h1></li>
+		<div style="float:right">
+<a href="#" class="fa fa-facebook"></a>
+<a href="#" class="fa fa-twitter"></a>
+<a href="#" class="fa fa-google"></a>
+<a href="#" class="fa fa-linkedin"></a>
+		</div>
+
+</ul>
+</div>
+<div class="icon-bar">
+  <a class="active" href="#"><i class="fa fa-home"></i></a> 
+  <a href="#"><i class="fa fa-search"></i></a> 
+  <a href="#"><i class="fa fa-envelope"></i></a> 
+  <a href="#"><i class="fa fa-globe"></i></a>
+  <a href="#"><i class="fa fa-file"></i></a> 
+</div>
+<div>
+	<div align="center">
+	<h2 style="color:#003366;font-family:Verdana;">Find your jobs here</h2>
+	</div>
+	
+
+
+
+</div>
+
+	</body>
+</html>
